80d19a20a6fcb904bca0a7adeffb4c1198ccae89
personalusevtc-CTC <personalusevtc@gmail.com>
2026-04-29 12:13:53 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,10 +13,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(256))
     
-    given_name = db.Column(db.String(64))
-    surname = db.Column(db.String(64))
-    gender = db.Column(db.String(10))
-    birth_date = db.Column(db.Date)
+    given_name = db.Column(db.String(64), nullable=False)
+    surname = db.Column(db.String(64), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
     
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
